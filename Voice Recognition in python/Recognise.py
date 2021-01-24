8b7cacd202b1b1c6f96847908fe1ba539b4375fb
Shraddha <shraddhaholay@gmail.com>
2021-01-24 19:54:44 +1300

    python text to speech

--- Voice Recognition in python/Recognise.py
+++ Voice Recognition in python/Recognise.py
@@ -1,17 +1,19 @@
 print('hello codes')
 import speech_recognition as sr
 import pyttsx3
-# print(dir(sr))
-# print('test')
 listener = sr.Recognizer()
-# print('test')
-while(True):
-        with sr.Microphone() as source:
-                print('listening...')
-                voice = listener.listen(source)
-                command = listener.recognize_google(voice)
-                # print(command)
-                command = command.lower()
-                if 'alexa' in command:
-                   print(command)
-
+engine = pyttsx3.init()
+engine.say('Hello, I am your Alexa , What can I do for you? Can I get some Dabeli today')
+while(1):
+        try:
+                with sr.Microphone() as source:
+                        print('listening...')
+                        voice = listener.listen(source)
+                        command = listener.recognize_google(voice)
+                        command = command.lower()
+                        if 'alexa' in command:
+                                engine.say(command)
+                                engine.runAndWait()
+                                print(command)
+        except:
+                print('error')
